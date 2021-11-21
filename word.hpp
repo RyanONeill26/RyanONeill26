6320217a37a481c9c336f2619ac045d032679efa
RyanONeill26 <94787573+RyanONeill26@users.noreply.github.com>
2021-11-21 12:47:09 +0000

    Delete word.hpp

--- word.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class word : public std::string 
-{
-  public:
-  word *next; // use this to link the next object in the linked list
-  int count; // use this to store count
-};
